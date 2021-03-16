37b46c40b31e04711e8b88ad7bf756e5462543b0
Patrick Tone <patrick@madronesoftware.com>
2021-03-16 13:33:49 +0100

    preparing for deployment

--- dist/tests/src/Main.js
+++
@@ -1,8 +0,0 @@
-import * as global$ from "../../../src/styles/global.scss";
-import { render } from "react-dom";
-import { createElement } from "react";
-import { HelloWorld } from "./App.js";
-
-
-render(createElement(HelloWorld, null), document.getElementById("feliz-app"));
-
