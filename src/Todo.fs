df260e19079f582a924763cbbf71e87a498db887
Patrick Tone <patrick@madronesoftware.com>
2021-03-16 10:04:52 +0100

    adds complete and delete

--- src/Todo.fs
+++ src/Todo.fs
@@ -5,29 +5,70 @@ open Feliz
 open Feliz.Bulma
 open Feliz.Bulma.Operators
 open Feliz.UseElmish
+open System
 
 open FontAwesome
 
+type private Todo =
+    { Id: Guid
+      Description: string
+      CompletedOn: Option<DateTime> }
+
 type private State =
-    { TodoList: string list
+    { TodoList: Todo list
       NewTodo: string }
 
 type private Msg =
     | SetNewTodo of string
     | AddNewTodo
+    | ToggleCompleted of Guid
+    | DeleteTodo of Guid
 
 let private init () =
-    { TodoList = [ "Learn F#" ]
-      NewTodo = "" },
+    { NewTodo = ""
+      TodoList =
+          [ { Id = Guid.NewGuid()
+              Description = "Learn F#"
+              CompletedOn = Some(DateTime.UtcNow.AddDays(-1.)) }
+            { Id = Guid.NewGuid()
+              Description = "Learn Elmish"
+              CompletedOn = None } ] },
     Cmd.none
 
 let private update msg state =
     match msg with
     | SetNewTodo todoText -> { state with NewTodo = todoText }, Cmd.none
+    | DeleteTodo todoId ->
+        let nextTodoList =
+            state.TodoList
+            |> List.filter (fun todo -> todo.Id <> todoId)
+
+        { state with TodoList = nextTodoList }, Cmd.none
+    | ToggleCompleted todoId ->
+        let nextTodoList =
+            state.TodoList
+            |> List.map
+                (fun todo ->
+                    if todo.Id = todoId then
+                        { todo with
+                              CompletedOn =
+                                  if todo.CompletedOn.IsNone then
+                                      Some(DateTime.UtcNow)
+                                  else
+                                      None }
+                    else
+                        todo)
+
+        { state with TodoList = nextTodoList }, Cmd.none
     | AddNewTodo when state.NewTodo = "" -> state, Cmd.none
     | AddNewTodo ->
+        let nextTodo =
+            { Id = Guid.NewGuid()
+              Description = state.NewTodo
+              CompletedOn = None }
+
         { state with
-              TodoList = List.append state.TodoList [ state.NewTodo ]
+              TodoList = List.append state.TodoList [ nextTodo ]
               NewTodo = "" },
         Cmd.none
 
@@ -52,10 +93,33 @@ let private inputField state dispatch =
                                                                                                 prop.children [ Html.i [ prop.classes [ FA.fas
                                                                                                                                         FA.fa_plus ] ] ] ] ] ] ] ]
 
+let private renderTodo todo dispatch =
+    Bulma.box [ Bulma.columns [ columns.isMobile ++ columns.isVCentered
+                                prop.children [ Bulma.column [ Bulma.text.p [ prop.className "subtitle"
+                                                                              prop.text todo.Description ] ]
+                                                Bulma.column [ column.isNarrow
+                                                               prop.children [ Bulma.buttons [ Bulma.button.button [ if todo.CompletedOn.IsSome then
+                                                                                                                         color.isSuccess
+                                                                                                                     prop.onClick
+                                                                                                                         (fun _ ->
+                                                                                                                             dispatch (
+                                                                                                                                 ToggleCompleted
+                                                                                                                                     todo.Id
+                                                                                                                             ))
+                                                                                                                     prop.children [ Html.i [ prop.classes [ FA.fas
+                                                                                                                                                             FA.fa_check ] ] ] ]
+                                                                                               Bulma.button.button [ color.isDanger
+                                                                                                                     prop.onClick
+                                                                                                                         (fun _ ->
+                                                                                                                             dispatch (
+                                                                                                                                 DeleteTodo
+                                                                                                                                     todo.Id
+                                                                                                                             ))
+                                                                                                                     prop.children [ Html.i [ prop.classes [ FA.fas
+                                                                                                                                                             FA.fa_trash ] ] ] ] ] ] ] ] ] ]
+
 let private todoList state dispatch =
-    Html.ul [ prop.children [ for todo in state.TodoList ->
-                                  Html.li [ prop.classes [ "box"; "subtitle" ]
-                                            prop.text todo ] ] ]
+    Html.ul [ prop.children [ for todo in state.TodoList -> renderTodo todo dispatch ] ]
 
 [<ReactComponent>]
 let App () =
